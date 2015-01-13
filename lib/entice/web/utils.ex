82fe6bab185e001a472155a98ffd2c9dada1cefa
ephe-meral <j.appel.online@gmail.com>
2015-01-13 16:10:37 +0100

    added bitwise seek op for bitstring

--- lib/entice/web/utils.ex
+++ lib/entice/web/utils.ex
@@ -7,4 +7,14 @@ defmodule Entice.Web.Utils do
   def copy_into(%{__struct__: result} = a, %{__struct__: _} = b) do
     struct(result, Dict.merge(Map.from_struct(a), Map.from_struct(b)))
   end
+
+  @doc """
+  Returns the bit of a bitstring at postion.
+  """
+  def bit_at(bin, i) when is_binary(bin) and i > 0 and i < bit_size(bin) do
+    seek = i - 1
+    rest = bit_size(bin) - i
+    <<_::size(seek), result::1, _::size(rest)>> = bin
+    result
+  end
 end
--- test/lib/entice/web/utils_test.exs
+++ test/lib/entice/web/utils_test.exs
@@ -8,4 +8,9 @@ defmodule Entice.Web.UtilsTest do
   test "struct copying" do
     assert copy_into(%B{}, %A{}) == %B{b: 2, c: 3, d: 3}
   end
+
+  test "bit at" do
+    assert 1 == bit_at(<<0b00010101>>, 4)
+    assert 0 == bit_at(<<0b00010101>>, 1)
+  end
 end
