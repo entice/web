a20645199d712e23ec3b54b52123fde292baac83
ephe-meral <j.appel.online@gmail.com>
2015-01-16 00:33:51 +0100

    improves bitwise ops

--- lib/entice/web/utils.ex
+++ lib/entice/web/utils.ex
@@ -13,33 +13,26 @@ defmodule Entice.Web.Utils do
   Returns the bit of a bitstring at postion.
   If the position is not in range of the bitstring, it assumes a value of 0.
   """
-  def bit_at(bin, i) when is_binary(bin) and i > 0 and i < bit_size(bin) do
-    seek = i - 1
-    rest = bit_size(bin) - i
-    <<_::size(rest), result::1, _::size(seek)>> = bin
-    result
+  def get_bit(n, b) do
+    case (n &&& bit_to_int(b)) > 0 do
+      true  -> 1
+      false -> 0
+    end
   end
 
-  def bit_at(bin, i) when is_binary(bin) and i > 0, do: 0
-
   @doc """
-  Sets a bit at specified position to true. This doesnt assume anything
+  Sets a bit at specified position. This doesnt assume anything
   about the binary - if it is to short, it will add zeroes until position is
   in range. Returns the new binary.
   """
-  def bit_set(<<0>>, i, value) when i > 0 do
-    << value <<< (i - 1) >>
-  end
+  def set_bit(n, b), do: n ||| bit_to_int(b)
 
-  def bit_set(bin, i, value) when is_binary(bin) and i > 0 and i < bit_size(bin) do
-    seek = i - 1
-    rest = bit_size(bin) - i
-    <<left::size(rest), _::1, right::size(seek)>> = bin
-    <<(left <<< i) ||| (value <<< (i-1)) ||| right>>
-  end
+  @doc """
+  Unsets a bit at specified position. This doesnt assume anything
+  about the binary - if it is to short, it will add zeroes until position is
+  in range. Returns the new binary.
+  """
+  def unset_bit(n, b), do: n &&& ~~~bit_to_int(b)
 
-  def bit_set(bin, i, value) when is_binary(bin) and i > 0 do
-    diff = i - bit_size(bin)
-    bit_set(<<0>>, diff, value) <> bin
-  end
+  defp bit_to_int(b), do: 1 <<< (b - 1)
 end
--- test/lib/entice/web/utils_test.exs
+++ test/lib/entice/web/utils_test.exs
@@ -10,21 +10,27 @@ defmodule Entice.Web.UtilsTest do
   end
 
   test "bit at" do
-    # in range            LSBit---v
-    assert 0 == bit_at(<<0b00010101>>, 4)
-    assert 1 == bit_at(<<0b00010101>>, 1)
-    assert 0 == bit_at(<<0b00010101>>, 8)
+    # in range
+    assert 0 == get_bit(0b00010101, 4)
+    assert 1 == get_bit(0b00010101, 1)
+    assert 0 == get_bit(0b00010101, 8)
     # outta range, mister!
-    assert 0 == bit_at(<<0b00010101>>, 9)
-    assert 0 == bit_at(<<0b00010101>>, 42)
+    assert 0 == get_bit(0b00010101, 9)
+    assert 0 == get_bit(0b00010101, 42)
   end
 
   test "bit set" do
     # in range
-    assert <<0b00001000>> == bit_set(<<0b00000000>>, 4, 1)
-    assert <<0b00000000>> == bit_set(<<0b00001000>>, 4, 0)
-    assert <<0b01101001>> == bit_set(<<0b01001001>>, 6, 1)
+    assert 0b00001000 == set_bit(0b00000000, 4)
+    assert 0b01101001 == set_bit(0b01001001, 6)
     # outta range
-    #assert <<0b101101001>> == bit_set(<<0b01101001>>, 9, 1)
+    assert 0b10001101001 == set_bit(0b01101001, 11)
+  end
+
+  test "bit unset" do
+    # in range
+    assert 0b00000000 == unset_bit(0b00001000, 4)
+    # outta range
+    assert 0b01101001 == unset_bit(0b01101001, 9)
   end
 end
