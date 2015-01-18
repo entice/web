f428385fb79e297b709b4e931211117a4dceb81e
ephe-meral <j.appel.online@gmail.com>
2015-01-18 18:24:59 +0100

    fix db migrations

--- priv/repo/migrations/20150109210153_init.exs
+++ priv/repo/migrations/20150109210153_init.exs
@@ -2,12 +2,14 @@ defmodule Entice.Web.Repo.Migrations.Init do
   use Ecto.Migration
 
   def up do
-    ["CREATE TABLE accounts( \
-        id serial PRIMARY KEY, \
-        email varchar(60) UNIQUE, \
-        password varchar(50))",
+    execute(
+      "CREATE TABLE accounts( \
+          id serial PRIMARY KEY, \
+          email varchar(60) UNIQUE, \
+          password varchar(50))")
 
-     "CREATE TABLE characters( \
+    execute(
+      "CREATE TABLE characters( \
         id serial PRIMARY KEY, \
         name varchar(30) UNIQUE, \
         profession int, \
@@ -18,16 +20,17 @@ defmodule Entice.Web.Repo.Migrations.Init do
         hair_color int, \
         hairstyle int, \
         face int, \
-        account_id int REFERENCES accounts)",
+        account_id int REFERENCES accounts)")
 
+    execute(
      "INSERT INTO accounts(email, password) \
       VALUES \
         ('root@entice.ps', 'root'), \
-        ('test@entice.ps', 'test')"]
+        ('test@entice.ps', 'test')")
   end
 
   def down do
-    ["DROP TABLE accounts",
-     "DROP TABLE characters"]
+    execute("DROP TABLE accounts")
+    execute("DROP TABLE characters")
   end
 end
--- priv/repo/migrations/20150111011829_add_char.exs
+++ priv/repo/migrations/20150111011829_add_char.exs
@@ -2,17 +2,19 @@ defmodule Entice.Web.Repo.Migrations.AddChar do
   use Ecto.Migration
 
   def up do
-    "INSERT INTO characters (name, profession, campaign, sex, height, skin_color, hair_color, hairstyle, face, account_id) \
-     VALUES \
-      ('Test Char', 1, 0, 1, 0, 3, 0, 7, 30,    (SELECT id FROM accounts WHERE email = 'root@entice.ps')), \
-      ('Shit Happens', 1, 0, 1, 0, 3, 0, 7, 30, (SELECT id FROM accounts WHERE email = 'root@entice.ps')), \
-      ('Cool Story', 1, 0, 1, 0, 3, 0, 7, 30,   (SELECT id FROM accounts WHERE email = 'test@entice.ps'))"
+    execute(
+      "INSERT INTO characters (name, profession, campaign, sex, height, skin_color, hair_color, hairstyle, face, account_id) \
+       VALUES \
+        ('Test Char', 1, 0, 1, 0, 3, 0, 7, 30,    (SELECT id FROM accounts WHERE email = 'root@entice.ps')), \
+        ('Shit Happens', 1, 0, 1, 0, 3, 0, 7, 30, (SELECT id FROM accounts WHERE email = 'root@entice.ps')), \
+        ('Cool Story', 1, 0, 1, 0, 3, 0, 7, 30,   (SELECT id FROM accounts WHERE email = 'test@entice.ps'))")
   end
 
   def down do
-    "DELETE FROM characters \
-     WHERE name = 'Test Char' \
-        OR name = 'Shit Happens' \
-        OR name = 'Cool Story'"
+    execute(
+      "DELETE FROM characters \
+       WHERE name = 'Test Char' \
+          OR name = 'Shit Happens' \
+          OR name = 'Cool Story'")
   end
 end
