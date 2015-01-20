c7d9237f62e4f8aa09ff14fc2c88438bd5f2ed0e
ephe-meral <j.appel.online@gmail.com>
2015-01-20 20:08:20 +0100

    initial goal is the spawn

--- lib/entice/web/players.ex
+++ lib/entice/web/players.ex
@@ -9,7 +9,7 @@ defmodule Entice.Web.Players do
     {:ok, id} = Entity.start(map, UUID.uuid4(), %{
       Name => %Name{name: char.name},
       Position => %Position{pos: map.spawn},
-      Movement => %Movement{},
+      Movement => %Movement{goal: map.spawn},
       Appearance => copy_into(%Appearance{}, char),
       SkillBar => %SkillBar{}})
     Groups.create_for(map, id)
