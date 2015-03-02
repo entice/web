5309d2c4abb89eed3e33aa00342a11d1459390bf
ephe-meral <j.appel.online@gmail.com>
2015-03-02 12:21:10 +0100

    bump logic version

--- mix.exs
+++ mix.exs
@@ -22,7 +22,7 @@ defmodule Entice.Web.Mixfile do
   #
   # Type `mix help deps` for examples and options
   defp deps do
-    [{:entice_logic, github: "entice/logic", ref: "fd9cfd25eba787d33314ed48e98833f00e507f5b"},
+    [{:entice_logic, github: "entice/logic", ref: "12f4ed56314310c0961addee4f54849c9339e092"},
      {:entice_entity, github: "entice/entity", ref: "88ca59ed636b321796f508273414358e9aa2a8a6"},
      {:entice_skill, github: "entice/skill", ref: "e663fc81868977c0deb6c322a4953d9a098411bf"},
      {:entice_utils, github: "entice/utils", ref: "4b743c4fe22eb4934221e69da2f50800347bcf32"},
