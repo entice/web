043d9f19282df722f34d5f3565a01d0e886ccd5e
Alexander Rummel <rummel.alexander@outlook.com>
2015-09-02 15:33:47 +0200

    Added create Account Stuff 	- Post Method 	- UI

---
+++ web/controllers/acc_controller.ex
@@ -0,0 +1,18 @@
+defmodule Entice.Web.AccountController do
+	use Phoenix.Controller
+	alias Entice.Web.Account
+	import Entice.Web.ControllerHelper
+
+	plug :action
+
+	def create(conn, _params) do
+		email = conn.params["email"]
+		password = conn.params["password"]
+
+		account = %Account{email: email, password: password}
+			|> Entice.Web.Repo.insert
+
+		conn |> json ok(%{
+			message: "Account created!"})
+	end
+end
