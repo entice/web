c9b275ec0b43d670e86e15ad7ea4407b57bd4f41
ephe-meral <j.appel.online@gmail.com>
2015-09-23 16:21:02 +0200

    fix broken sys env usage

--- config/prod.exs
+++ config/prod.exs
@@ -2,7 +2,9 @@ use Mix.Config
 
 config :entice_web, Entice.Web.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: {:system, "HOST_NAME"} || "to.entice.so", port: {:system, "HOST_PORT"} || 80],
+  url: [
+    host: (System.get_env("HOST_NAME") || "to.entice.so"),
+    port: (System.get_env("HOST_PORT") || 80)],
   cache_static_manifest: "priv/static/manifest.json"
 
 # Do not print debug messages in production
