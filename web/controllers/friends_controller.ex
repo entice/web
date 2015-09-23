19a5a9af8d12279a86cc0fe9c88d51667a7b965a
ephe-meral <j.appel.online@gmail.com>
2015-09-23 15:43:00 +0200

    cleanup and dont escape token link in client view

--- web/controllers/friends_controller.ex
+++ web/controllers/friends_controller.ex
@@ -7,9 +7,9 @@ defmodule Entice.Web.FriendsController do
 
   @doc "Returns all friends of connected account."
   def index(conn, _params) do
-    id = get_session(conn, :client_id)    
+    id = get_session(conn, :client_id)
     {:ok, friends} = Entice.Web.Client.get_friends(id)
-    
+
     friends = for friend <- friends, do: friend.friend_account.id
     conn |> json ok%{
       message: "All friends",
@@ -25,21 +25,21 @@ defmodule Entice.Web.FriendsController do
     friend_account_id = conn.params["friend_account_id"]
 
     friend_account = Entice.Web.Repo.get(Entice.Web.Account, friend_account_id)
-    
+
     result = case friend_account do
       nil -> error(%{message: "This account does not exist."})
       %Account{id: ^account_id} -> error(%{message: "Can't add yourself."})
       _ ->
         case Queries.get_friend(account_id, friend_account_id) do
-          {:error, :no_matching_friend} -> 
+          {:error, :no_matching_friend} ->
             Queries.add_friend(acc, friend_account)
-            ok%{message: "Friend added."}              
-          {:ok, friend} -> error%{message: "Already in friends list."}
-        end  
-    end    
+            ok%{message: "Friend added."}
+          {:ok, _friend} -> error%{message: "Already in friends list."}
+        end
+    end
     conn |> json result
   end
-      
+
   @doc "Deletes friend :id from friends list of connected account."
   def delete(conn, _params) do
     session_id = get_session(conn, :client_id)
