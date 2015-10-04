d625d3457a9ba1189e107d285d9d714d7520a1fa
EtienneDesticourt <etienne.desticourt@github.com>
2015-10-05 00:04:09 +0200

    Implemented token and docu controller tests

--- test/web/controllers/docu_controller_test.exs
+++ test/web/controllers/docu_controller_test.exs
@@ -1,52 +1,61 @@
-# defmodule Entice.Web.DocuControllerTest do
-#   use Entice.Web.ConnCase
+defmodule Entice.Web.DocuControllerTest do
+  use Entice.Web.ConnCase
 
-#   setup context do
-#     result = %{}
-#     result = case context.id do
-#       0 -> Map.put(result, :params, %{})
-#       1 -> Map.put(result, :params, %{})
-#       2 -> Map.put(result, :params, %{})
-#       3 -> Map.put(result, :params, %{})
-#       _ -> Map.put(result, :params, %{})
-#     end
-#     {:ok, result}
-#   end
+  setup context do
+    result = %{email: "root@entice.ps", password: "root" }
+    result = case context.id do
+      0 -> Map.put(result, :params, %{})
+      1 -> Map.put(result, :params, %{"id" => "1"})
+      2 -> Map.put(result, :params, %{})
+      3 -> Map.put(result, :params, %{})
+      _ -> Map.put(result, :params, %{})
+    end
+    {:ok, result}
+  end
 
-#   @tag id: 0
-#   test "maps write case here", context do
-#     {:ok, result} = fetch_route(:get, "/api/maps", context)
+  @tag id: 0
+  test "maps success", context do
+    {:ok, result} = fetch_route(:get, "/api/maps", context)
 
-#     assert result["status"] == "ok", "maps should have succeeded but didn't."
-#     assert result["message"] ==  "All maps...", "maps returned unexpected value for key: message."
-#     assert result["maps"] ==  maps, "maps returned unexpected value for key: maps."
-#   end
+    assert result["status"] == "ok", "maps should have succeeded but didn't."
+    assert result["message"] ==  "All maps...", "maps returned unexpected value for key: message."
+    assert result["maps"] != [], "maps returned unexpected value for key: maps."
+  end
 
-#   @tag id: 1
-#   test "skills write case here", context do
-#     {:ok, result} = fetch_route(:get, "/api/skills", context)
 
-#     assert result["status"] == "error", "skills should have failed but didn't."
-#     assert result["message"] ==  m, "skills returned unexpected value for key: message."
-#   end
 
-#   @tag id: 2
-#   test "skills write case here", context do
-#     {:ok, result} = fetch_route(:get, "/api/skills/:id", context)
+  #Finish tests below once latest skill branch is merged
+  #No points in fixing the route until then
 
-#     assert result["status"] == "ok", "skills should have succeeded but didn't."
-#     assert result["message"] ==  "Requested skill...", "skills returned unexpected value for key: message."
-#     assert result["skill"] ==  %{            id, "skills returned unexpected value for key: skill."
-#     assert result["name"] ==  s.underscore_name, "skills returned unexpected value for key: name."
-#     assert result["description"] ==  s.description}, "skills returned unexpected value for key: description."
-#   end
 
-#   @tag id: 3
-#   test "skills write case here", context do
-#     {:ok, result} = fetch_route(:get, "/api/skills", context)
+  #Currently fails as it's supposed to
+  #get_skill returns a skill but
+  #/api/skills calls it expecting {:error, :message} or {:ok, skill}
+  # @tag id: 1
+  # test "skills wrong id", context do
+  #   {:ok, result} = fetch_route(:get, "/api/skills", context)
 
-#     assert result["status"] == "ok", "skills should have succeeded but didn't."
-#     assert result["message"] ==  "All skills...", "skills returned unexpected value for key: message."
-#     assert result["skill"] ==  sk, "skills returned unexpected value for key: skill."
-#   end
-# end
+  #   assert result["status"] == "error", "skills should have failed but didn't."
+  #   assert result["message"] ==  "", "skills returned unexpected value for key: message."
+  # end
+
+  # @tag id: 2
+  # test "skills write case here", context do
+  #   {:ok, result} = fetch_route(:get, "/api/skills/:id", context)
+
+  #   assert result["status"] == "ok", "skills should have succeeded but didn't."
+  #   assert result["message"] ==  "Requested skill...", "skills returned unexpected value for key: message."
+  #   assert result["skill"] ==  %{            id, "skills returned unexpected value for key: skill."
+  #   assert result["name"] ==  s.underscore_name, "skills returned unexpected value for key: name."
+  #   assert result["description"] ==  s.description}, "skills returned unexpected value for key: description."
+  # end
+
+  # @tag id: 3
+  # test "skills write case here", context do
+  #   {:ok, result} = fetch_route(:get, "/api/skills", context)
+
+  #   assert result["status"] == "ok", "skills should have succeeded but didn't."
+  #   assert result["message"] ==  "All skills...", "skills returned unexpected value for key: message."
+  #   assert result["skill"] ==  sk, "skills returned unexpected value for key: skill."
+  # end
+end
--- test/web/controllers/token_controller_test.exs
+++ test/web/controllers/token_controller_test.exs
@@ -1,25 +1,19 @@
-# defmodule Entice.Web.TokenControllerTest do
-#   use Entice.Web.ConnCase
+defmodule Entice.Web.TokenControllerTest do
+  use Entice.Web.ConnCase
 
-#   setup context do
-#     result = %{}
-#     result = case context.id do
-#       0 -> Map.put(result, :params, %{})
-#       _ -> Map.put(result, :params, %{})
-#     end
-#     {:ok, result}
-#   end
+  setup do
+    result = %{email: "root@entice.ps", password: "root" }
+    result = Map.put(result, :params, %{map: "heroes_ascent", char_name: "Test Char"})
+    {:ok, result}
+  end
 
-#   @tag id: 0
-#   test "entity_token write case here", context do
-#     {:ok, result} = fetch_route(:get, "/api/token/entity", context)
+  @tag id: 0
+  test "entity_token success", context do
+    {:ok, result} = fetch_route(:get, "/api/token/entity", context)
 
-#     assert result["status"] == "ok", "entity_token should have succeeded but didn't."
-#     assert result["message"] ==  "Transferring...", "entity_token returned unexpected value for key: message."
-#     assert result["client_id"] ==  id, "entity_token returned unexpected value for key: client_id."
-#     assert result["entity_id"] ==  eid, "entity_token returned unexpected value for key: entity_id."
-#     assert result["entity_token"] ==  token, "entity_token returned unexpected value for key: entity_token."
-#     assert result["map"] ==  map_mod.underscore_name, "entity_token returned unexpected value for key: map."
-#     assert result["is_outpost"] ==  map_mod.is_outpost?, "entity_token returned unexpected value for key: is_outpost."
-#   end
-# end
+    assert result["status"] == "ok", "entity_token should have succeeded but didn't."
+    assert result["message"] ==  "Transferring...", "entity_token returned unexpected value for key: message."
+    assert result["map"] ==  "heroes_ascent", "entity_token returned unexpected value for key: map."
+    assert result["is_outpost"] ==  true, "entity_token returned unexpected value for key: is_outpost."
+  end
+end
