be8d0dfe80f77520fcb5c8b745309e8b837ad7c0
EtienneDesticourt <etienne.desticourt@github.com>
2015-10-18 01:12:19 +0200

    Updated handle_in("cast) to work with casting

--- mix.exs
+++ mix.exs
@@ -22,8 +22,8 @@ defmodule Entice.Web.Mixfile do
   defp elixirc_paths(_),     do: ["lib", "web"]
 
   defp deps do
-    [{:entice_logic, github: "entice/logic", ref: "c907ca1f28899e715e1bdbdf167f9b67370db80c"},
-     {:entice_entity, github: "entice/entity", ref: "f39a63a0e73bcddc39b9562316fb03e8bda53e8c"},
+    [{:entice_logic, github: "entice/logic", ref: "04c5ccc9c0fda1372880f73a42fef99d8e6fda88"},
+     {:entice_entity, github: "entice/entity", ref: "6d952892c56c2a8c636baca143e64f43c553e1b9"},
      {:entice_utils, github: "entice/utils", ref: "74ce9f8a2e2fd7766263e193bffba4901aa425a8"},
      {:cowboy, "~> 1.0"},
      {:phoenix, "~> 1.0.3"},
--- web/channels/skill_channel.ex
+++ web/channels/skill_channel.ex
@@ -4,6 +4,7 @@ defmodule Entice.Web.SkillChannel do
   alias Entice.Logic.Skills
   alias Entice.Logic.Area
   alias Entice.Logic.SkillBar
+  alias Entice.Logic.Casting
   alias Phoenix.Socket
 
 
@@ -17,9 +18,30 @@ defmodule Entice.Web.SkillChannel do
 
   def handle_info(:after_join, %Socket{assigns: %{entity_id: entity_id, character: char}} = socket) do
     Coordination.register_observer(self)
-    :ok = SkillBar.register(entity_id, char.skillbar)
+    SkillBar.register(entity_id, char.skillbar)
+    Casting.register(entity_id)
     socket |> push("initial", %{unlocked_skills: char.available_skills, skillbar: entity_id |> SkillBar.get_skills})
     {:noreply, socket}
+  end
+
+  def handle_info(:skill_casted, info, socket) do
+    socket |> broadcast("cast:end", info)
+    {:reply, :ok, socket}
+  end
+
+  def handle_info(:skill_cast_interrupted, info, socket) do
+    socket |> broadcast("cast:interrupted", info)
+    {:reply, :ok, socket}
+  end
+
+  def handle_info(:skill_recharged, info, socket) do
+    socket |> broadcast("recharge:end", info)
+    {:reply, :ok, socket}
+  end
+
+  def handle_info(:after_cast_delay_ended, info, socket) do
+    socket |> broadcast("delay:ended", info)
+    {:reply, :ok, socket}
   end
 
   def handle_info(_msg, socket), do: {:noreply, socket}
@@ -44,38 +66,37 @@ defmodule Entice.Web.SkillChannel do
     end
   end
 
-
-  def handle_in("cast", %{"slot" => slot}, socket) when slot in 0..10 do
-    cast_callback = fn skill ->
-      Entice.Web.Endpoint.broadcast(socket.topic, "cast:end", %{
-        entity: socket |> entity_id,
-        slot: slot,
-        skill: skill.id,
-        recharge_time: skill.recharge_time})
-    end
-    recharge_callback = fn skill ->
-      Entice.Web.Endpoint.broadcast(socket.topic, "recharge:end", %{
-        entity: socket |> entity_id,
-        slot: slot,
-        skill: skill.id})
-    end
-    case socket |> entity_id |> SkillBar.cast_skill(slot, cast_callback, recharge_callback) do
+  def handle_in("cast", %{"slot" => slot, "target" => target}, socket) when slot in 0..10 do
+    skill = Skillbar.get_skill(socket |> entity_id, slot)
+    case socket |> entity_id |> Casting.cast_skill(skill, slot, target, self) do
       {:error, reason} -> {:reply, {:error, %{slot: slot, reason: reason}}, socket}
-      {:ok, :normal, skill} ->
+      {:ok, skill, _cast_time} ->
         socket |> broadcast("cast:start", %{
           entity: socket |> entity_id,
           slot: slot,
           skill: skill.id,
           cast_time: skill.cast_time})
         {:reply, :ok, socket}
-      {:ok, :instant, skill} ->
-        socket |> broadcast("cast:instantly", %{
-          entity: socket |> entity_id,
-          slot: slot,
-          skill: skill.id,
-          recharge_time: skill.recharge_time})
-        {:reply, :ok, socket}
     end
+
+    #Will be useful for instantaneous implemntation i imagine
+    # case socket |> entity_id |> SkillBar.cast_skill(slot, cast_callback, recharge_callback) do
+    #   {:error, reason} -> {:reply, {:error, %{slot: slot, reason: reason}}, socket}
+    #   {:ok, :normal, skill} ->
+    #     socket |> broadcast("cast:start", %{
+    #       entity: socket |> entity_id,
+    #       slot: slot,
+    #       skill: skill.id,
+    #       cast_time: skill.cast_time})
+    #     {:reply, :ok, socket}
+    #   {:ok, :instant, skill} ->
+    #     socket |> broadcast("cast:instantly", %{
+    #       entity: socket |> entity_id,
+    #       slot: slot,
+    #       skill: skill.id,
+    #       recharge_time: skill.recharge_time})
+    #     {:reply, :ok, socket}
+    # end
   end
 
 
