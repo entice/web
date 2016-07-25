7a2d268d713ac5fe779b47d7460d165a0da6341a
Kukkaistyttö <kukka@crypto-keupone.eu>
2016-07-25 18:02:52 +0200

    refactoring

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Entice.Web.Mixfile do
   def project do
     [app: :entice_web,
      version: "0.0.1",
-     elixir: "~> 1.2",
+     elixir: "~> 1.3",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
      build_embedded: Mix.env == :prod,
